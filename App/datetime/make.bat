3e8b00d8fcac753df648af27c53d563aadb1c9fd
Sergey A <a-s-m@km.ru>
2016-09-13 16:05:19 +0300

    new file:   App/datetime/DATE.COM 	new file:   App/datetime/DATE.MAC 	new file:   App/datetime/SETCLOCK.COM 	new file:   App/datetime/SETCLOCK.MAC 	new file:   App/datetime/TIME.COM 	new file:   App/datetime/TIME.MAC 	new file:   App/datetime/make.bat

---
+++ App/datetime/make.bat
@@ -0,0 +1,12 @@
+cpm m80n TIME,=TIME
+cpm l80m TIME,TIME/n/e
+del TIME.rel
+
+cpm m80n DATE,=DATE
+cpm l80m DATE,DATE/n/e
+del DATE.rel
+
+cpm m80n SETCLOCK,=SETCLOCK
+cpm l80m SETCLOCK,SETCLOCK/n/e
+del SETCLOCK.rel
+
