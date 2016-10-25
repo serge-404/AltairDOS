9f5a5362c892f7638b78b5e5db1128b0a6c40be5
Sergey A <a-s-m@km.ru>
2016-10-25 17:05:42 +0300

    more universal version of PACK.BIN 	modified:   App/source/bitbuster/make.bat

--- App/source/bitbuster/make.bat
+++ App/source/bitbuster/make.bat
@@ -1,2 +1,2 @@
-for %%i in (*.com) do echo %%i & pack %%i & copy /b DEPACK.BIN + %%i.pck .\ready\%%i & del %%i.tmp.pck
+for %%i in (*.com) do echo %%i & pack %%i & copy /b DEPACK.BIN + %%i.pck .\ready\%%i & del %%i.pck
 
